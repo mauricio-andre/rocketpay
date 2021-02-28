96a03ee4e0e17722eef14deed73df0db3e1c6a2c
dev03 <mauricio.r.andre@gmail.com>
2021-02-28 14:30:54 -0300

    Terceira aula, tabela e rota account

--- src/lib/rocketpay_web/controllers/accounts_controller.ex
+++ src/lib/rocketpay_web/controllers/accounts_controller.ex
@@ -1,9 +1,17 @@
-defmodule RocketpayWeb.UsersController do
+defmodule RocketpayWeb.AccountsController do
   use RocketpayWeb, :controller
 
-  alias Rocketpay.User
+  alias Rocketpay.{User, Account}
 
   action_fallback RocketpayWeb.FallbackController
+
+  def deposit(conn, params) do
+    with {:ok, %Account{} = account} <- Rocketpay.deposit(params) do
+      conn
+      |> put_status(:ok)
+      |> render("update.json", account: account)
+    end
+  end
 
   def create(conn, params) do
     with {:ok, %User{} = user} <- Rocketpay.create_user(params) do
