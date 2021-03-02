1d00a05d93eff0f1baed03f456521355fff0b961
dev03 <mauricio.r.andre@gmail.com>
2021-03-01 21:02:20 -0300

    Quinta aula, testes em controller views e mais

---
+++ src/test/rocketpay_web/views/users_view_test.exs
@@ -0,0 +1,37 @@
+defmodule RocketpayWeb.UsersViewTest do
+  use RocketpayWeb.ConnCase, async: true
+
+  import Phoenix.View
+
+  alias Rocketpay.{User, Account}
+  alias RocketpayWeb.UsersView
+
+  test "renders create.json" do
+    params = %{
+      name: "Mauricio",
+      password: "123456",
+      nickname: "MauMau",
+      email: "mauricio@gmail.com",
+      age: 27
+    }
+
+    {:ok, %User{id: user_id, account: %Account{id: account_id}} = user} = Rocketpay.create_user(params)
+
+    response = render(UsersView, "create.json", user: user)
+
+    expected_response = %{
+      message: "User created",
+      user: %{
+        account: %{
+          balance: Decimal.new("0.00"),
+          id: account_id
+        },
+        id: user_id,
+        name: "Mauricio",
+        nickname: "MauMau"
+      }
+    }
+
+    assert expected_response == response
+  end
+end
